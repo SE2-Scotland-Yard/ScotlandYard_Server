928d0128afb221f979ca871930c048250be33123
noxar007 <manuel.stingl@gmx.at>
2025-06-03 16:37:20 +0200

    fixed Method getMrXHistory

--- src/main/java/at/aau/serg/scotlandyard/gamelogic/GameState.java
+++ src/main/java/at/aau/serg/scotlandyard/gamelogic/GameState.java
@@ -371,7 +371,7 @@ public class GameState {
 
     public List<String> getMrXMoveHistory() {
         List<String> history = new ArrayList<>();
-        for (int i = 1; i < roundManager.getCurrentRound(); i++) {
+        for (int i = 1; i <= roundManager.getCurrentRound(); i++) {
             MrXMove move = mrXHistory.get(i);
             if (move == null) continue;
 
